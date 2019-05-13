d70cc396e14ac9f8b1dc8fd0b339d5d5f7091323
KyleBlackwell01 <48078043+KyleBlackwell01@users.noreply.github.com>
2019-05-13 10:55:44 +1000

    TODO

---
+++ term2/week11/scripts.js
@@ -0,0 +1,130 @@
+var contentP = document.getElementById("contentHere");
+var buttonC = document.getElementById("ButtonC");
+var buttonD = document.getElementById("ButtonD");
+// var buttonA = document.getElementById("ButtonA");
+var myInput = document.getElementById("myInput");
+
+
+    // var items = document.querySelectorAll("#contentHere li")
+    //     tab = [], contentP;
+
+    //     for(var i = 0; i < items.length; i++) {
+    //         tab.push(items[i].innerHTML);
+    //     }
+
+    //     for(var i = 0; i < items.length; i++) {
+    //         items[i].onclick = function(){
+
+    //             contentP = tab.indexOf(this.innerHTML);
+    //             console.log(this.innerHTML + " index = " + contentP);
+    //         }
+    //     }
+
+
+//buttonA.onclick = removeAll;
+buttonC.onclick = ourButtonClick;
+buttonD.onclick = removeItem;
+
+
+function ourButtonClick()
+{
+    if (myInput.value == "")
+    {
+        alert("Input is required!");
+        return
+    }
+    else
+    {
+
+        var x = document.getElementsByTagName("LI")[0];
+        x.setAttribute("id", "text");
+        myInput.setAttribute('id',myInput.value);
+
+        contentP.innerHTML += "<li>" + myInput.value + "</li>";
+    }
+}
+
+// function ourButtonClick()
+// {
+//     create.setAttribute('id',myInput.value);
+//     create.appendChild(document.createTextNode(myInput.value));
+//     contentP.appendChild(create);
+// }
+
+// function removeItem()
+// {
+//     contentP.removeChild(contentP.childNodes[0]);
+// }
+
+// function removeAll()
+// {
+//     contentP.removeChild(contentP.childNodes);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// var myNodelist = document.getElementsByTagName("LI");
+//  var i;
+//  for (i = 0; i < contentP.length; i++) {
+//    var span = document.createElement("SPAN");
+//    var txt = document.createTextNode("\u00D7");
+//    span.className = "close";
+//    span.appendChild(txt);
+//    contentP[i].appendChild(span);
+//  }
+ 
+//  // Click on a close button to hide the current list item
+//  var close = document.getElementsByClassName("close");
+//  var i;
+//  for (i = 0; i < close.length; i++) {
+//    close[i].onclick = function() {
+//      var div = this.parentElement;
+//      div.style.display = "none";
+//    }
+//  }
+ 
+//  // Add a "checked" symbol when clicking on a list item
+//  var list = document.querySelector('ul');
+//  list.addEventListener('click', function(ev) {
+//    if (ev.target.tagName === 'LI') {
+//      ev.target.classList.toggle('checked');
+//    }
+//  }, false);
+ 
+ // Create a new list item when clicking on the "Add" button
+//  function newElement() {
+//    var li = document.createElement("li");
+//    var inputValue = document.getElementById("myInput").value;
+//    var t = document.createTextNode(inputValue);
+//    li.appendChild(t);
+//    if (inputValue === '') {
+//      alert("You must write something!");
+//    } else {
+//      document.getElementById("contentHere").appendChild(li);
+//    }
+//    document.getElementById("myInput").value = "";
+
+//    var span = document.createElement("SPAN");
+//    var txt = document.createTextNode("\u00D7");
+//    span.className = "close";
+//    span.appendChild(txt);
+//    li.appendChild(span);
+ 
+
+ 
+//    for (i = 0; i < close.length; i++) {
+//      close[i].onclick = function() {
+//        var div = this.parentElement;
+//        div.style.display = "none";
+//      }
+//    }
+//  } 
