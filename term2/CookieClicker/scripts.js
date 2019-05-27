eb879cd94325edd7cf024faeecb42610739dcfdb
KyleBlackwell01 <48078043+KyleBlackwell01@users.noreply.github.com>
2019-05-27 10:37:31 +1000

    Added Cookie image for clicks

--- term2/CookieClicker/scripts.js
+++ term2/CookieClicker/scripts.js
@@ -1,9 +1,18 @@
 var clicker = document.getElementById("clicker");
 var counter = document.getElementById("counter");
 
+
 counter.value = 0;
+
+
+clicker.onsubmit = function(event){
+    event.preventDefault();
+};
+
 clicker.addEventListener("click", function(){
+    event.preventDefault();
+
     console.log();
     counter.value = counter.value + 1;
-    counter.innerHTML = counter.value;
+    counter.innerHTML = "Total: " + counter.value;
 });
